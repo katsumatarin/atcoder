91020381414009c9d598c3a880b0c46968b56280
katsumatarin <nir.k021017@gmail.com>
2020-09-20 17:44:25 +0900

    abc043 b問題のバグ修正

--- abc043/b/b.go
+++ abc043/b/b.go
@@ -8,15 +8,17 @@ func main() {
 	for i := 0; i < len(s); i++ {
 		if s[i:i+1] == "0" {
 			a += "0"
-			fmt.Println(a)
+			// fmt.Println(a)
 		} else if s[i:i+1] == "1" {
 			a += "1"
-			fmt.Println(a)
+			// fmt.Println(a)
 		} else {
 			sc := []rune(a)
-			a := ""
-			a += string(sc[:(len(sc) - 1)])
-			fmt.Println(a)
+			a = ""
+			if len(sc) > 0{
+				a += string(sc[:(len(sc) - 1)])
+			}
+			// fmt.Println(a)
 		}
 	}
 	fmt.Println(a)
