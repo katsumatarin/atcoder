5a868b8b82d1b9d9ee903a8a35d665fc8eb80401
Ryosuke Miyake <ryosuke@Ryosuke-mbp.local>
2020-09-20 23:42:34 +0900

    文字列読み取りのサンプルを作成

--- abc042/b/b.go
+++ abc042/b/b.go
@@ -10,14 +10,16 @@ var sc = bufio.NewScanner(os.Stdin)
 
 
 
-func nextLine() string {
-	sc.Scan()
-	return sc.Text()
-}
 
 func main() {
 	var n, l int
+	var s[100] string
 	fmt.Scan(&n, &l)
-	s := nextLine()
-	fmt.Println(s)
+	for i := 0; i < n; i++{
+		fmt.Scan(&s[i])
+	}
+
+	for i := 0; i < n; i++{
+		fmt.Printf("%s\n", s[i])
+	}
 }
