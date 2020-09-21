9d6d88d5ccf8d0e8709e9559673f7b8764c67d60
katsumatarin <nir.k021017@gmail.com>
2020-09-21 18:25:33 +0900

    abc042 b問題

--- abc042/b/b.go
+++ abc042/b/b.go
@@ -4,22 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
+
+type StringList []string
+
+func (a StringList) Len() int           { return len(a) }
+func (a StringList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a StringList) Less(i, j int) bool { return a[i] < a[j] }
 
 var sc = bufio.NewScanner(os.Stdin)
 
-
-
-
 func main() {
 	var n, l int
-	var s[100] string
+	var s StringList = []string{}
+	var ans string = "";
+
 	fmt.Scan(&n, &l)
 	for i := 0; i < n; i++{
-		fmt.Scan(&s[i])
+		var inputString string
+		fmt.Scan(&inputString)
+		s = append(s, inputString)
 	}
 
-	for i := 0; i < n; i++{
-		fmt.Printf("%s\n", s[i])
+	sort.Sort(s)
+
+	for  i := 0; i < n; i++ {
+		ans += s[i]
 	}
+
+	fmt.Printf("%s\n", ans)
 }
