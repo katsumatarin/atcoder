1c860216136b4c2c17b64ecd1270dd49cd7c296f
katsumatarin <nir.k021017@gmail.com>
2020-11-26 21:56:38 +0900

    abc060 b問題

---
+++ abc060/b/b.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+	for i := 1; i <= b; i++ {
+		if a*i%b == c {
+			fmt.Println("YES")
+			return
+		}
+	}
+	fmt.Println("NO")
+}
