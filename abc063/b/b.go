76dae6255ff4978ab4ab4cbced096ca4c3ff91e5
katsumatarin <nir.k021017@gmail.com>
2020-12-04 18:19:33 +0900

    abc063 B問題

---
+++ abc063/b/b.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var s string
+	w := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	fmt.Scan(&s)
+	for i := 0; i < len (w); i++ {
+		if strings.Count(s, w[i]) >= 2 {
+			fmt.Println("no")
+			return
+		}
+	}
+	fmt.Println("yes")
+}
