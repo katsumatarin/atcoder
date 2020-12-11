a148484322d43373d7d497536c6e533fada6c4b3
katsumatarin <nir.k021017@gmail.com>
2020-12-11 11:47:15 +0900

    abc063 b問題

--- abc063/b/b.go
+++ abc063/b/b.go
@@ -9,7 +9,7 @@ func main() {
 	var s string
 	w := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 	fmt.Scan(&s)
-	for i := 0; i < len (w); i++ {
+	for i := 0; i < len(w); i++ {
 		if strings.Count(s, w[i]) >= 2 {
 			fmt.Println("no")
 			return
