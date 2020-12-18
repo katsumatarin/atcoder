c57f837bffa265b923da7bb4028731abb070e2dc
katsumatarin <nir.k021017@gmail.com>
2020-12-18 18:57:06 +0900

    APG4b K問題

---
+++ ex10/10.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  printf("A:");
+  int i = 0;
+  while (i < A) {
+    cout << "]";
+    i++;
+  }
+  cout << endl;
+
+  printf("B:");
+  i = 0;
+  while (i < B) {
+    cout << "]";
+    i++;
+  }
+  cout << endl;
+}
