658795cd9391bd7bbfcf2f2f03a8ddf4554e7bd8
katsumatarin <nir.k021017@gmail.com>
2020-12-18 18:32:56 +0900

    APG4b I問題

---
+++ ex8/8.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int p;
+  int price;
+  string text;
+  cin >> p;
+
+  // パターン1
+  if (p == 1) {
+    cin >> price;
+  }
+
+  // パターン2
+  if (p == 2) {
+    cin >> text >> price;
+  }
+
+  int N;
+  cin >> N;
+
+  // パターン1
+  if (p == 1) {
+    cout << price * N << endl;
+  }
+
+  // パターン2
+  if (p == 2) {
+    cout << text << "!" << endl;
+    cout << price * N << endl;
+  }
+
+}
