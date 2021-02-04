ac6fb6480def2dddae156c477595d9f1ee2aadc1
katsumatarin <nir.k021017@gmail.com>
2021-02-04 22:30:15 +0900

    m-solutions2020 c問題

--- m-solutions2020/c/c.cpp
+++ m-solutions2020/c/c.cpp
@@ -2,18 +2,32 @@
 using namespace std;
 
 int main() {
-  int N, K;
+  int64_t N, K;
   cin >> N >> K;
-  vector<int> vec;
-  int A;
-  for (int i = 0; i < N; i++) {
-    cin >> A;
-    vec.push_back(A);
+  int64_t A[1 << 18];
+  for (int i = 1; i <= N; i++) {
+    cin >> A[i];
   }
-  int sum = 0;
-  for (int i = K; i < N + 1; i++) {
-    for (int j = i; j > K; j--) {
-      sum = vec[];
-    }
+  for (int i = K + 1; i <= N; i++) {
+    if (A[i-K] < A[i]) cout << "Yes" << endl;
+    else cout << "No" << endl;
   }
+  return 0;
+  // int64_t ans = 0;
+  // vector<int> ansvec;
+  // for (int i = K; i < N + 1; i++) {
+  //   int64_t sum = 1;
+  //   for (int j = K-K; j < K; j++) {
+  //     sum *= vec[j+i-K];
+  //   }
+  //   ansvec.push_back(sum);
+  //   if (i != K) {
+  //     if(ansvec[i-K] > ansvec[i-K-1]){
+  //       cout << "Yes" << endl;
+  //     } else {
+  //       cout << "No" << endl;
+  //     }
+  //   }
+  // }
+
 }
