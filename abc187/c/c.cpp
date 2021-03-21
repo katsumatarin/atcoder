a597e27333920ec214c48580e9b0644d5bc68bea
katsumatarin <nir.k021017@gmail.com>
2021-03-21 12:36:02 +0900

    abc187 c問題

--- abc187/c/c.cpp
+++ abc187/c/c.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
   int64_t N;
   cin >> N;
+
   string S;
-  vector<string> vec1;
-  vector<string> vec2;
+  vector<string> vec1, vec2;
   for (int64_t i = 0; i < N; i++) {
     cin >> S;
     char c = S[0];
@@ -19,36 +19,21 @@ int main() {
   }
 
   sort(vec1.begin(), vec1.end());
+  vec1.erase(unique(vec1.begin(), vec1.end()), vec1.end());
   sort(vec2.begin(), vec2.end());
+  vec2.erase(unique(vec2.begin(), vec2.end()), vec2.end());
 
-  if (vec1.size() < vec2.size()) {
-    for (int i = 0; i < vec1.size(); i++) {
-      for (int j = 0; j < vec2.size(); j++) {
-        if (vec1[i][0] == vec2[j][0]) {
-          if (vec1[i] == vec2[j]) {
-            cout << vec1[i] << endl;
-            return 0;
-          }
-        } else {
-          break;
-        }
-      }
-    }
-  } else {
-    for (int i = 0; i < vec2.size(); i++) {
-      for (int j = 0; j < vec1.size(); j++) {
-        if (vec2[i][0] == vec1[j][0]) {
-          if (vec2[i] == vec1[j]) {
-            cout << vec2[i] << endl;
-            return 0;
-          }
-        } else {
-          break;
-        }
-      }
+  vec1.insert(vec1.end(), vec2.begin(), vec2.end());
+
+  int vec1_size = vec1.size();
+  sort(vec1.begin(), vec1.end());
+
+  for(int i = 0; i < vec1_size - 1; i++) {
+    if(vec1[i] == vec1[i+1]) {
+      cout << vec1[i] << endl;
+      return 0;
     }
   }
-
   cout << "satisfiable" << endl;
-
+  return 0;
 }
