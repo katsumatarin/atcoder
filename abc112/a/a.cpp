510fb72452153a92591806edefb908f863d24b6d
katsumatarin <nir.k021017@gmail.com>
2021-03-31 18:43:12 +0900

    abc112 a問題

---
+++ abc112/a/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  cin>>N;
+  int A, B;
+  if(N == 1){
+    cout<<"Hello World"<<endl;
+  }
+  else {
+    cin>>A>>B;
+    cout<<A+B<<endl;
+  }
+}
