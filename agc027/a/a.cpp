e60d07f6effd022a19dcef107f26c928b50dc35f
katsumatarin <nir.k021017@gmail.com>
2021-04-08 17:10:16 +0900

    agc027 a

--- agc027/a/a.cpp
+++ agc027/a/a.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
   int N, x;
   cin >> N >> x;
+
   vector<int> vec;
   int a = 0, sum = 0, ans = 0;
+
   for(int i = 0; i < N; i++){
     cin >> a;
     sum += a;
@@ -14,12 +16,22 @@ int main() {
 
   sort(vec.begin(), vec.end());
 
-  if (vec[0] > x) {
-    cout << ans << endl;
+  if(sum == x) {
+    ans = N;
   }
 
-  if (sum <= x) {
-    ans = N;
-    cout << ans << endl;
+  if (sum < x) {
+    ans = N - 1;
   }
+
+  int k = 0;
+  if (sum > x) {
+    while(x >= vec[k]){
+      x -= vec[k];
+      k++;
+    }
+    ans = k;
+  }
+
+  cout << ans << endl;
 }
