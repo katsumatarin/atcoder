ffcaff8255d459fecae1b66c1da2d55655204fb3
katsumatarin <nir.k021017@gmail.com>
2021-04-24 14:28:51 +0900

    abc170 c問題

--- abc170/c/c.cpp
+++ abc170/c/c.cpp
@@ -2,25 +2,33 @@
 using namespace std;
 
 int main() {
-  int X, N;
+  int X, N, p;
   cin >> X >> N;
-  vector<int> small;
-  vector<int> big;
+
+  vector<int> v;
   bool existX = false;
-  int p;
   for(int i = 0; i < N; i++) {
     cin >> p;
-    if(p < X) {
-      small.push_back(p);
-    } else if(p > X) {
-      big.push_back(p);
-    } else if(p == X) {
-      existX = true;
-    }
+    v.push_back(p);
+    if(p == X) existX = true;
   }
 
   if(existX == false) {
     cout << X << endl;
+    return 0;
   }
 
+
+  for(int i = 1; i <= 100; i++){
+    auto result1 = find(v.begin(), v.end(), X - i);
+    if (result1 == v.end()) {
+      cout << X - i << endl;
+      return 0;
+    }
+    auto result2 = find(v.begin(), v.end(), X + i);
+    if (result2 == v.end()) {
+      cout << X + i << endl;
+      return 0;
+    }
+  }
 }
