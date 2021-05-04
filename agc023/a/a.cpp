811b307b996ccd248337937a7d94a3e694ed6bfe
katsumatarin <nir.k021017@gmail.com>
2021-05-04 17:34:15 +0900

    agc023 a問題　

--- agc023/a/a.cpp
+++ agc023/a/a.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int main(){
-  int n;
+  long long n;
   cin >> n;
-  vector<int> a;
-  vector<int> b;
+  vector<long long> a;
+  vector<long long> b;
   b.push_back(0);
 
-  int A;
-  for(int i = 0; i < n; i++){
+  long long A;
+  for(long long i = 0; i < n; i++){
     cin >> A;
     a.push_back(A);
   }
 
-  for(int i = 1; i < n+1; i++){
+  for(long long i = 1; i < n+1; i++){
     b.push_back(a[i-1] + b[i-1]);
   }
 
   sort(b.begin(), b.end());
 
-  int sum = 1, ans = 0;
-  for(int i = 0; i < b.size(); i++){
+  long long sum = 1, ans = 0;
+  for(long long i = 0; i < (long long)(b.size()) - 1; i++){
     if(b[i] == b[i+1]){
       sum++;
     } else {
@@ -31,6 +31,8 @@ int main(){
       }
     }
   }
+
+  ans += sum * (sum-1) / 2;
 
   cout << ans << endl;
 }
