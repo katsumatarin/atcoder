0c16ef613c027bb5cb9812289962c2c66a35bb48
katsumatarin <nir.k021017@gmail.com>
2021-05-12 00:29:08 +0900

    tenlka1-2017-beginner

---
+++ tenka1-2017-beginner/a/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  string s;
+  cin >> s;
+  int ans = 0;
+  for(int i = 0; i < s.length(); i++) if(s[i] == '1') ans++;
+  cout << ans << endl;
+  return 0;
+}
