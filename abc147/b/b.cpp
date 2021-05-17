9381971beed66aba17f69422790272aab989b012
katsumatarin <nir.k021017@gmail.com>
2021-05-17 13:17:17 +0900

    abc147 b問題

---
+++ abc147/b/b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  string s;
+  cin >> s;
+  int cnt = 0;
+  for(int i = 0; i < s.length()/2; i++) if(s[i] != s[s.length()-1-i]) cnt++;
+  cout << cnt << endl;
+  return 0;
+}
