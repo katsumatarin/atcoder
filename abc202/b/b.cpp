11a1f7becc3afcda3a9e867d2fc558a937d1a4d2
katsumatarin <nir.k021017@gmail.com>
2021-05-22 22:58:37 +0900

    abc202 b問題

---
+++ abc202/b/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  string s;
+  cin >> s;
+  string ans;
+  for(int i = 0; i < s.length(); i++){
+    if(s[s.length()-1-i] == '9') {
+      ans += '6';
+    } else if (s[s.length()-1-i] == '6') {
+      ans += '9';
+    } else {
+      ans += s[s.length()-1-i];
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
