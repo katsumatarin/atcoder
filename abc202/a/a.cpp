8cb2b8547685b0650383a42d840955efe226adbe
katsumatarin <nir.k021017@gmail.com>
2021-05-22 22:51:58 +0900

    abc202 a問題

---
+++ abc202/a/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  int a, b, c;
+  cin >> a >> b >> c;
+  int ans = (7-a) + (7-b) + (7-c);
+  cout << ans << endl;
+  return 0;
+}
